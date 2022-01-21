a8f71380f97a0efdcb5a3e9572c25e85eb9324d7
Nowy Condro <nowyc@spotify.com>
2022-01-21 09:45:44 +0100

    [TOOLING] 4. Add winston

--- src/app.js
+++ src/app.js
@@ -7,8 +7,25 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 const endpoints = require('./endpoints');
+const logger = require('./logger')();
+
+// Very simple implementation logger middleware
+function errorLoggerMiddleware(req, res, next) {
+    const originalSend = res.send;
+    res.send = function(data) {
+        // Currently only log the response that has error_code
+        if (data.error_code) {
+            logger.error(`${req.method} ${req.url} ${data.error_code} ${data.message}`);
+        }
+        res.send = originalSend;
+        return res.send(data);
+    }
+    next();
+}
 
 module.exports = (db) => {
+    app.use(errorLoggerMiddleware);
+
     app.get('/health', endpoints.healthGetHandler);
 
     app.post('/rides', jsonParser, endpoints.ridesPostHandler.bind(null, db));
